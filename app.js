50d2ab7d6d3e37e69e08afa9b8c37f8834868fe3
Josh Buckner <joshgbuckner@gmail.com>
2019-01-23 17:38:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const _ = require('lodash');
+const multer = require('multer');
+
+const upload = multer({ dest: 'public/uploads'});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use('/maps', express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/fortniteMapsDB', {useNewUrlParser: true});
+
+const mapsSchema = {
+	name: String,
+	author: String,
+	code: String,
+	photo: String,
+	category: String,
+	date: Date
+};
+
+const Map = mongoose.model("Map", mapsSchema);
+
+
+
+
+app.get('/', function(req, res) {
+	Map.find({}, function(err, foundMaps) {
+		if(!err) {
+			
+			res.render('home', { tilesDisplay: foundMaps, headingDisplay: "Fortnite Creative Codes" });
+		}
+	});
+});
+
+app.get('/maps/:mapName', function(req, res) {
+	const requestedMap = req.params.mapName;
+
+
+	Map.find({}, function(err, foundMaps) {
+		foundMaps.forEach(function(map) {
+			
+			const storedCode = map.code;
+
+			if (requestedMap === storedCode) {
+				res.render('userMap', { map: map, headingDisplay: map.category });
+				// console.log(map);
+			}
+		});
+	});
+});
+
+app.get('/all', function(req, res) {
+	Map.find({}, function(err, foundMaps) {
+		if(!err) {
+			res.render('maps', { tilesDisplay: foundMaps, headingDisplay: "All Maps" });
+		}
+	});
+});
+
+app.get('/obstacle-parkour', function(req, res) {
+	Map.find({category: "obstacle-parkour"}, function(err, foundMaps) {
+		if(!err) {
+			res.render('maps', { tilesDisplay: foundMaps, headingDisplay: "Obstacle & Parkour Maps" });
+		}
+	});
+});
+
+app.get('/racing', function(req, res) {
+	Map.find({category: "racing"}, function(err, foundMaps) {
+		if(!err) {
+			res.render('maps', { tilesDisplay: foundMaps, headingDisplay: "Racing Maps" });
+		}
+	});
+});
+
+app.get('/multiplayer', function(req, res) {
+	Map.find({category: "multiplayer"}, function(err, foundMaps) {
+		if(!err) {
+			res.render('maps', { tilesDisplay: foundMaps, headingDisplay: "Multiplayer Maps" });
+		}
+	});
+});
+
+app.get('/edit-courses', function(req, res) {
+	Map.find({category: "edit-courses"}, function(err, foundMaps) {
+		if(!err) {
+			res.render('maps', { tilesDisplay: foundMaps, headingDisplay: "Edit Courses" });
+		}
+	});
+});
+
+app.get('/creative-builds', function(req, res) {
+	Map.find({category: "creative-builds"}, function(err, foundMaps) {
+		if(!err) {
+			res.render('maps', { tilesDisplay: foundMaps, headingDisplay: "Creative Builds" });
+		}
+	});
+});
+
+app.get('/submit', function(req, res) {
+	// console.log("submit clicked");
+	res.render('submit', {headingDisplay: "Submit A Map"});
+});
+
+app.post('/submit', upload.single('mapPhoto'), function(req, res) {
+	const mapName = req.body.mapName;
+	const authorName = req.body.authorName;
+	const islandCode = req.body.islandCode;
+	const category = req.body.category;
+	const date = new Date();
+
+	// const 
+	// host = req.host;
+	const filePath = req.file.path.substring(7);
+	// console.log(filePath);
+
+
+	if (!req.file) {
+		console.log("no file recieved");
+	} else {
+		console.log('file received');
+	}
+
+	const map = new Map({
+		name: mapName,
+		author: authorName,
+		code: islandCode,
+		photo: filePath,
+		category: category,
+		date: date
+	});
+
+	
+
+	Map.find({code: islandCode}, function(err, foundMaps) {
+		if(!err) {
+			if (foundMaps.length !== 0) {
+				console.log("Map already in database");
+			} else {
+				console.log("Map is okay to add");
+				console.log(map);
+				// map.save();
+				// res.redirect('/maps/' + map.code);
+				// , { map: map, headingDisplay: map.name }
+			}
+		}
+	});
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
---
+++ public/js/script.js
@@ -0,0 +1,87 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+	'use strict'
+
+	window.addEventListener('load', function() {
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = $('.needs-validation')
+
+		// Loop over them and prevent submission
+		Array.prototype.filter.call(forms, function(form) {
+			form.addEventListener('submit', function(event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault()
+					event.stopPropagation()
+				}
+				form.classList.add('was-validated')
+			}, false)
+		})
+	}, false)
+}())
+
+function generateModalData(title, image, code, author,) {
+	console.log(title, image, code, author)
+	$('#modal-title')[0].innerHTML = title;
+	$('#modal-image').attr("src", image);
+	$('#modal-code')[0].innerHTML = code;
+	$('#modal-author')[0].innerHTML = author;
+}
+
+function previewFile() {
+  var preview = $(".file-preview");
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    preview[0].src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } 
+  $(".custom-file-label")[0].innerText = document.querySelector('input[type=file]').files[0].name;
+}
+
+// function insertHyphens() {
+// 	$('#island-code')[0].value = $('#island-code')[0].value.replace(/(\d{4})(\d{4})(\d{4})/, "$1-$2-$3");
+	
+// }
+
+function addHyphen() {
+	let ele = $('#island-code')[0];
+    ele = ele.value.split('-').join('');    // Remove dash (-) if mistakenly entered.
+
+    let finalVal = ele.match(/.{1,4}/g).join('-');
+
+    $('#island-code')[0].value = finalVal;
+}
+
+
+// console.log($('.map-name')[0].checkValidity());
+
+$('#myForm').on('submit', function(e){
+	$('#modal-title')[0].innerHTML = $('#map-name')[0].value;
+	$('#modal-author')[0].innerHTML = $('#author')[0].value;
+	$('#modal-code')[0].innerHTML = $('#island-code')[0].value;
+	$('#modal-image').attr("src", $('#image-preview')[0].src);
+
+  	// e.preventDefault();
+  	
+  	if ($('#map-name')[0].checkValidity() && 
+  		$('#author')[0].checkValidity() && 
+  		$('#island-code')[0].checkValidity() && 
+  		$('#inputGroupFile01')[0].checkValidity() && 
+  		$('#category')[0].checkValidity()) {
+			$('#myModal').modal('show');
+	}
+	// resetForm();
+	// setTimeout(function() {
+	// 	resetForm();
+	// });
+});
+
+function resetForm() {
+	document.getElementById('myForm').reset();
+	$('#image-preview')[0].src = "images/fortnite_creative_2.jpg";
+	$(".custom-file-label")[0].innerText = "Choose Photo";
+}
